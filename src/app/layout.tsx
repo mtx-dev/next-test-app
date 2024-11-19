90cbfb6e089d655e92e540a80b15a18fd7412c80
Andrew Kozlitskiy <andrey.kozlitskiy@gmail.com>
2024-11-19 17:21:23 +0100

    Clear files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body>
         <Header />
         <main className="container mx-auto p-4">{children}</main>
